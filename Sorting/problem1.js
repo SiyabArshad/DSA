4382b89afe31a6cb2a00cc71d2cbc441e37fde7a
SiyabArshad <siyabarshadsatti@gmail.com>
2024-03-27 13:46:56 +0500

    Bubble sort implemented

---
+++ Sorting/problem1.js
@@ -0,0 +1,15 @@
+// bubble sort
+const bubbleSort = (arr = []) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([13, 46, 24, 52, 20, 9]));
